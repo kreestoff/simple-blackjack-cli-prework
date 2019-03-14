583b14c90d8b2c20cefcc3df3f930cb0383d0a01
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-14 19:00:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,11 +28,11 @@ def initial_round
   return tot
 end
 
-def hit?
+def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input == "h"
-    
+    card_total += deal_card
     
 end
 
