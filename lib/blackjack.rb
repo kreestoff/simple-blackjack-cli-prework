1e3b2f40bda6e2eb4a6dcde871c6b12287abdd59
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-14 19:02:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,12 +35,12 @@ def hit?(card_total)
     card_total += deal_card
   elsif user_input == "s"
     card_total
-  else 
-    
+  else invalid_command
+  end 
 end
 
 def invalid_command
-  # code invalid_command here
+  
 end
 
 #####################################################
